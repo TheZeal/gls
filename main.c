bd62bcb29a4de208735c09bf223ed99b32b752ef
Zeal / Nalynia <zeal@ZealBook.stark.lan>
2016-11-09 22:08:57 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  gls
+//
+//  Created by Zeal Iskander on 09/11/2016.
+//  Copyright © 2016 Zeal Iskander. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
